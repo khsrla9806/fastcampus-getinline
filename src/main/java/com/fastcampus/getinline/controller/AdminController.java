cc7e0f9f40594ff76ac3390f21608c3fb7806631
khsrla9806 <kimhunsope@naver.com>
2023-04-12 17:46:10 +0900

    Feat: admin/places에서 쿼리 파라미터로 검색어가 될 수 있는 값 추가

--- src/main/java/com/fastcampus/getinline/controller/AdminController.java
+++ src/main/java/com/fastcampus/getinline/controller/AdminController.java
@@ -1,17 +1,32 @@
 package com.fastcampus.getinline.controller;
 
+import com.fastcampus.getinline.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
 
     @GetMapping("/places")
-    public String adminPlaces() {
-        return "admin/places";
+    public ModelAndView adminPlaces(
+            PlaceType placeType, // 검색어를 추가
+            String placeName,
+            String address
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeType", placeType);
+        map.put("placeName", placeName);
+        map.put("address", address);
+
+        return new ModelAndView("admin/places", map);
     }
 
     @GetMapping("/places/{placeId}")
