958a0fe0ac6f85b050760db439955568bc8acd54
khsrla9806 <kimhunsope@naver.com>
2023-04-14 15:24:10 +0900

    Feat: Spring 내부 Exception을 처리하기 위한 코드 추가
    1. ResponseEntityExceptionHandler 상속
    2. handlerExceptionInternal() 메서드를 오버라이드 하여 Body에 APIErrorResponse 넣어주기
    3. 기존에 사용하던 ExceptionHandler에서도 handlerExceptionInternal()을 사용하여 return할 수 있도록 설정

--- src/main/java/com/fastcampus/getinline/controller/error/APIExceptionController.java
+++ src/main/java/com/fastcampus/getinline/controller/error/APIExceptionController.java
@@ -3,43 +3,70 @@ package com.fastcampus.getinline.controller.error;
 import com.fastcampus.getinline.constant.ErrorCode;
 import com.fastcampus.getinline.dto.APIErrorResponse;
 import com.fastcampus.getinline.exception.GeneralException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static com.fastcampus.getinline.constant.ErrorCode.INTERNAL_ERROR;
 
 @RestControllerAdvice(annotations = RestController.class) // RestController를 쓰는 클래스로 범위를 좁힘
-public class APIExceptionController {
+public class APIExceptionController extends ResponseEntityExceptionHandler {
     @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> general(GeneralException exception) {
+    public ResponseEntity<Object> general(GeneralException exception, WebRequest request) {
         ErrorCode errorCode = exception.getErrorCode();
         HttpStatus status = errorCode.isClientSideError() ?
                 HttpStatus.BAD_REQUEST :
                 HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return ResponseEntity
-                .status(status)
-                .body(APIErrorResponse.of(
-                        false,
-                        errorCode,
-                        errorCode.getMessage(exception.getMessage())
-                ));
+        // ResponseEntityExceptionHandler의 기능을 사용할 수 있도록 설정해주는 Return
+        return super.handleExceptionInternal(
+                exception,
+                APIErrorResponse.of(false, errorCode, errorCode.getMessage(exception)),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
+
+//        return ResponseEntity
+//                .status(status)
+//                .body(APIErrorResponse.of(
+//                        false,
+//                        errorCode,
+//                        errorCode.getMessage(exception.getMessage())
+//                ));
     }
 
     @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> exception(Exception exception) {
+    public ResponseEntity<Object> exception(Exception exception, WebRequest request) {
         ErrorCode errorCode = INTERNAL_ERROR;
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return ResponseEntity
-                .status(status)
-                .body(APIErrorResponse.of(
-                        false,
-                        errorCode,
-                        errorCode.getMessage(exception.getMessage())
-                ));
+        return super.handleExceptionInternal(
+                exception,
+                APIErrorResponse.of(false, errorCode, errorCode.getMessage(exception)),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
+
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorCode errorCode = status.is4xxClientError() ?
+                ErrorCode.SPRING_BAD_REQUEST :
+                ErrorCode.SPRING_INTERNAL_ERROR;
+        return super.handleExceptionInternal(
+                ex,
+                APIErrorResponse.of(false, errorCode, errorCode.getMessage(ex)),
+                headers,
+                status,
+                request
+        );
     }
 }
