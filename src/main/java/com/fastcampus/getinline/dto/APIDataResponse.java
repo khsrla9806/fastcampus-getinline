3b80cb729913a189aa844ee67e80ffbffcaa5962
khsrla9806 <kimhunsope@naver.com>
2023-04-18 00:18:52 +0900

    Refactor: APIDataResponse를 Type Safe하도록 Generic 사용

--- src/main/java/com/fastcampus/getinline/dto/APIDataResponse.java
+++ src/main/java/com/fastcampus/getinline/dto/APIDataResponse.java
@@ -1,5 +1,6 @@
 package com.fastcampus.getinline.dto;
 
+import com.fastcampus.getinline.constant.ErrorCode;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -7,15 +8,15 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode(callSuper = true)
-public class APIDataResponse extends APIErrorResponse {
-    private final Object data;
+public class APIDataResponse<T> extends APIErrorResponse {
+    private final T data;
 
-    private APIDataResponse(Boolean success, Integer errorCode, String message, Object data) {
-        super(success, errorCode, message);
+    private APIDataResponse(T data) {
+        super(true, ErrorCode.OK.getCode(), ErrorCode.OK.getMessage());
         this.data = data;
     }
 
-    public static APIDataResponse of(Boolean success, Integer errorCode, String message, Object data) {
-        return new APIDataResponse(success, errorCode, message, data);
+    public static <T> APIDataResponse<T> of(T data) {
+        return new APIDataResponse<>(data);
     }
 }
