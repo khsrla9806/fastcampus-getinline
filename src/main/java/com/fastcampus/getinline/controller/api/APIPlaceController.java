1c3b05e0b0754d20165cf1027aa096b2b5d74d9f
khsrla9806 <kimhunsope@naver.com>
2023-04-18 00:22:42 +0900

    Feat: Test를 위한 APIPlaceController 코드 변경
    - Return 타입을 List<String>에서 APIDataResponse<List<PlaceDto>>로 변경
    - getPlaces()와 getPlace()에 테스트 성공을 위한 return 데이터 넣어주는 코드 추가 (추후에 삭제)

--- src/main/java/com/fastcampus/getinline/controller/api/APIPlaceController.java
+++ src/main/java/com/fastcampus/getinline/controller/api/APIPlaceController.java
@@ -1,5 +1,9 @@
 package com.fastcampus.getinline.controller.api;
 
+import com.fastcampus.getinline.constant.PlaceType;
+import com.fastcampus.getinline.domain.Place;
+import com.fastcampus.getinline.dto.APIDataResponse;
+import com.fastcampus.getinline.dto.PlaceDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -8,8 +12,15 @@ import java.util.List;
 @RestController
 public class APIPlaceController {
     @GetMapping("/places")
-    public List<String> getPlaces() {
-        return List.of("place1", "place2");
+    public APIDataResponse<List<PlaceDto>> getPlaces() {
+        return APIDataResponse.of(List.of(PlaceDto.of(
+                PlaceType.COMMON,
+                "패스트캠퍼스",
+                "서울시 강남구 강남대로 1234",
+                "010-1234-1234",
+                30,
+                "신장개업"
+        )));
     }
 
     @PostMapping("/places")
@@ -18,8 +29,19 @@ public class APIPlaceController {
     }
 
     @GetMapping("/places/{placeId}")
-    public String getPlace(@PathVariable Integer placeId) {
-        return "place " + placeId;
+    public APIDataResponse<PlaceDto> getPlace(@PathVariable Integer placeId) {
+        if (placeId.equals(2)) {
+            return APIDataResponse.of(null); // 테스트를 위한 임시 코드
+        }
+
+        return APIDataResponse.of(PlaceDto.of(
+                PlaceType.COMMON,
+                "패스트캠퍼스",
+                "서울시 강남구 강남대로 1234",
+                "010-1234-1234",
+                30,
+                "신장개업"
+        ));
     }
 
     @PutMapping("/places/{placeId}")
