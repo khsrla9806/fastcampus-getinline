ef451f75aa184c90156c27e7f9f21c48637d1ed9
khsrla9806 <kimhunsope@naver.com>
2023-04-21 23:52:42 +0900

    Feat: Exception Handler에서 ConstraintViolationException 처리 코드 추가
    (1) ResponseEntityExceptionHandler를 상속받아서 사용합니다.
    (2) handlerExceptionInternal을 사용해주면 return 값이 ResponseEntity가 나오기 때문에 사용

--- src/main/java/com/fastcampus/getinline/controller/error/APIExceptionController.java
+++ src/main/java/com/fastcampus/getinline/controller/error/APIExceptionController.java
@@ -3,16 +3,43 @@ package com.fastcampus.getinline.controller.error;
 import com.fastcampus.getinline.constant.ErrorCode;
 import com.fastcampus.getinline.dto.APIErrorResponse;
 import com.fastcampus.getinline.exception.GeneralException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
 
 import static com.fastcampus.getinline.constant.ErrorCode.INTERNAL_ERROR;
 
 @RestControllerAdvice(annotations = RestController.class) // RestController를 쓰는 클래스로 범위를 좁힘
-public class APIExceptionController {
+public class APIExceptionController extends ResponseEntityExceptionHandler {
+
+    /**
+     * ResponseEntityExceptionHandler를 사용하는 예외처리 코드로 작성
+     */
+    @ExceptionHandler
+    public ResponseEntity<Object> validation(ConstraintViolationException exception, WebRequest request) {
+        ErrorCode errorCode = ErrorCode.VALIDATION_ERROR;
+        HttpStatus status = HttpStatus.BAD_REQUEST; // 이미 Client-Side인 것을 알기 떄문에
+
+        return super.handleExceptionInternal(
+                exception,
+                APIErrorResponse.of(
+                        false,
+                        errorCode,
+                        errorCode.getMessage(exception)
+                ),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
+    }
+
     @ExceptionHandler
     public ResponseEntity<APIErrorResponse> general(GeneralException exception) {
         ErrorCode errorCode = exception.getErrorCode();
