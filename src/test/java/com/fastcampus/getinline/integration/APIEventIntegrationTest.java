acade3e72e8a84ea8ae1d7d5bb0a1dc399801eb3
khsrla9806 <kimhunsope@naver.com>
2023-04-26 17:41:30 +0900

    Feat: 통합 테스트에서 test 프로필 활성화되도록 설정
    (1) @ActiveProfiles("test")를 붙여서 통합테스트에서는 test 프로필 설정을 사용할 수 있도록 설정

--- src/test/java/com/fastcampus/getinline/integration/APIEventIntegrationTest.java
+++ src/test/java/com/fastcampus/getinline/integration/APIEventIntegrationTest.java
@@ -7,17 +7,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+@ActiveProfiles("test") // application.properties에 있는 설정과는 상관없이 원하는 profile 설정으로 테스트가 가능하다.
 @AutoConfigureMockMvc // mvc를 AutoWired하기 위한 애노테이션
 @SpringBootTest
 public class APIEventIntegrationTest {
