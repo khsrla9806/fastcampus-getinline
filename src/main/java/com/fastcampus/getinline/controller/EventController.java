6227ee3b68af3440e127609854c7eaeeee79d39d
khsrla9806 <kimhunsope@naver.com>
2023-04-27 18:04:56 +0900

    Feat: ModelAndView를 이용하여 html 파일로 데이터 전송

--- src/main/java/com/fastcampus/getinline/controller/EventController.java
+++ src/main/java/com/fastcampus/getinline/controller/EventController.java
@@ -1,21 +1,75 @@
 package com.fastcampus.getinline.controller;
 
+import com.fastcampus.getinline.constant.EventStatus;
+import com.fastcampus.getinline.dto.EventDto;
+import com.fastcampus.getinline.dto.EventResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/events")
 @Controller
 public class EventController {
 
-    @GetMapping("/")
-    public String events() {
-        return "event/index";
+    @GetMapping("")
+    public ModelAndView events() {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("events", List.of(
+                EventDto.of(
+                        1L,
+                        "오후 운동",
+                        EventStatus.OPENED,
+                        LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                        LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                        0,
+                        24,
+                        "마스크 꼭 착용하세요",
+                        LocalDateTime.now(),
+                        LocalDateTime.now()
+                ),
+                EventDto.of(
+                        1L,
+                        "오후 운동",
+                        EventStatus.OPENED,
+                        LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                        LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                        0,
+                        24,
+                        "마스크 꼭 착용하세요",
+                        LocalDateTime.now(),
+                        LocalDateTime.now()
+                )
+        ));
+
+        return new ModelAndView("event/index", map);
     }
 
     @GetMapping("/{eventId}")
-    public String eventDetail(@PathVariable Long eventId) {
-        return "event/detail";
+    public ModelAndView eventDetail(@PathVariable Long eventId) {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("event", EventDto.of(
+                1L,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                0,
+                24,
+                "마스크 꼭 착용하세요",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        ));
+
+        return new ModelAndView("event/detail", map);
     }
 }
