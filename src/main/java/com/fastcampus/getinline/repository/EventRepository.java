7c7d499bd87b23cc537d6883a73b486b2e1f0590
khsrla9806 <kimhunsope@naver.com>
2023-05-02 23:17:54 +0900

    Feat: Spring Data JPA, H2 db 의존성 추가 및 사용을 위한 프로퍼티 설정

--- src/main/java/com/fastcampus/getinline/repository/EventRepository.java
+++ src/main/java/com/fastcampus/getinline/repository/EventRepository.java
@@ -1,14 +1,16 @@
 package com.fastcampus.getinline.repository;
 
 import com.fastcampus.getinline.constant.EventStatus;
+import com.fastcampus.getinline.domain.Event;
 import com.fastcampus.getinline.dto.EventDto;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 // TODO: 나중에 default 뺄거임
-public interface EventRepository {
+public interface EventRepository extends JpaRepository<Event, Long> {
     default List<EventDto> findEvents(
             Long placeId,
             String eventName,
