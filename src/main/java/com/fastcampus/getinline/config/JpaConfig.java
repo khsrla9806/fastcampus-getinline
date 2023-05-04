9147afc4567381448bf6b4ad1b8c996467ac83b9
khsrla9806 <kimhunsope@naver.com>
2023-05-04 23:17:16 +0900

    refactor: JpaConfig 원상복구 (지금은 수동 설정이 필요하지 않음)

--- src/main/java/com/fastcampus/getinline/config/JpaConfig.java
+++ src/main/java/com/fastcampus/getinline/config/JpaConfig.java
@@ -1,51 +1,10 @@
 package com.fastcampus.getinline.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 @EnableJpaAuditing // Auditing 기능을 추가
 @Configuration
 public class JpaConfig {
-    // H2 데이터베이스 수동설정하여 DataSource Bean 등록
-    @ConfigurationProperties("spring.datasource")
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
-//    @Bean
-//    public DataSource dataSource() {
-//        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-//        return builder.setType(EmbeddedDatabaseType.H2).build();
-//    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
-
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPackagesToScan("com.fastcampus.getinline");
-        factory.setDataSource(dataSource);
-        return factory;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-
-        JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(entityManagerFactory);
-        return txManager;
-    }
+    // 지금은 Properties 설정으로 모두 되기 때문에 설정 원상복구
 }
