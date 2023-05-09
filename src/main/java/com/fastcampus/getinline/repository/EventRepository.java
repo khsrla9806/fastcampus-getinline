765035270f27e1f60a2df9443469abaabd0375be
khsrla9806 <kimhunsope@naver.com>
2023-05-09 22:17:09 +0900

    feat: QuerydslBinderCustomizer 인터페이스의 customize 메소드 구현 추가

--- src/main/java/com/fastcampus/getinline/repository/EventRepository.java
+++ src/main/java/com/fastcampus/getinline/repository/EventRepository.java
@@ -1,10 +1,26 @@
 package com.fastcampus.getinline.repository;
 
 import com.fastcampus.getinline.domain.Event;
+import com.fastcampus.getinline.domain.QEvent;
+import com.querydsl.core.types.dsl.ComparableExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 public interface EventRepository extends
         JpaRepository<Event, Long>,
-        QuerydslPredicateExecutor<Event> {
+        QuerydslPredicateExecutor<Event>,
+        QuerydslBinderCustomizer<QEvent> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QEvent root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.place.placeName, root.eventName, root.eventStatus, root.eventStartDatetime, root.eventEndDatetime);
+        bindings.bind(root.place.placeName).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.eventName).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.eventStartDatetime).first(ComparableExpression::goe);
+        bindings.bind(root.eventEndDatetime).first(ComparableExpression::loe);
+    }
 }
--- src/main/java/com/fastcampus/getinline/repository/PlaceRepository.java
+++ src/main/java/com/fastcampus/getinline/repository/PlaceRepository.java
@@ -1,11 +1,24 @@
 package com.fastcampus.getinline.repository;
 
 import com.fastcampus.getinline.domain.Place;
+import com.fastcampus.getinline.domain.QPlace;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 public interface PlaceRepository extends
         JpaRepository<Place, Long>,
-        QuerydslPredicateExecutor<Place> {
+        QuerydslPredicateExecutor<Place>,
+        QuerydslBinderCustomizer<QPlace> {
 
+    @Override
+    default void customize(QuerydslBindings bindings, QPlace root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.placeName, root.address, root.phoneNumber);
+        bindings.bind(root.placeName).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.address).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.phoneNumber).first(StringExpression::containsIgnoreCase);
+    }
 }
